7292f6c971d0a341a62fb2c9e40118dd126c83ab
Jonathan Echavarria <jrec93@gmail.com>
2016-02-19 22:34:00 -0500

    newline fix

--- enumeration/shodan/shodan_ip_info.sh
+++ enumeration/shodan/shodan_ip_info.sh
@@ -1,3 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SHODAN_API_KEY=$(cat $DIR/SHODAN_API_KEY)
 curl -H "Accept: application/json" "https://api.shodan.io/shodan/host/$1?key=$SHODAN_API_KEY" 
+printf '\n'
