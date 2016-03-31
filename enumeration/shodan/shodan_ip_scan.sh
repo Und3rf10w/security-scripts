8506425861df9e2900cf15340a63b5ca408e359b
Jonathan Echavarria <jrec93@gmail.com>
2016-03-31 17:01:24 -0400

    bugfixes

--- enumeration/shodan/shodan_ip_scan.sh
+++ enumeration/shodan/shodan_ip_scan.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SHODAN_API_KEY=$(cat $DIR/SHODAN_API_KEY)
-curl -H 'Accept: application/json' "https://api.shodan.io/shodan/scan?key=$SHODAN_API_KEY" -d "ips=$1"  | python -mjson.tool
+curl --silent -H 'Accept: application/json' "https://api.shodan.io/shodan/scan?key=$SHODAN_API_KEY" -d "ips=$1"  | python -mjson.tool
 printf '\n'
